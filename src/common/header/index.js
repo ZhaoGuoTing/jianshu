f5bb4bea94d54fe75ade7afdceeadc53c4e4db25
ZhaoGuoTing <zhaoguoting@aliyun.com>
2019-06-21 19:40:26 +0800

    P7-2

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
-import { HeaderWrapper } from './style'
+import {
+  HeaderWrapper,
+  Logo, Nav, NavItem
+} from './style'
 
 export class Header extends Component {
   render() {
     return (
       <HeaderWrapper>
-        Header
+        <Logo href='/' />
+        <Nav>
+          <NavItem className='left'>首页</NavItem>
+          <NavItem className='left'>下载App</NavItem>
+          <NavItem className='right'>登录</NavItem>
+          <NavItem className='right'>Aa</NavItem>
+        </Nav>
       </HeaderWrapper>
     )
   }
