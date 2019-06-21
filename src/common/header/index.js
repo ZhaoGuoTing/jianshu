2c68b57aa4513fdb6191c80084b4bdcc96308204
ZhaoGuoTing <zhaoguoting@aliyun.com>
2019-06-21 18:04:28 +0800

    安装styled-components使用

---
+++ src/common/header/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { HeaderWrapper } from './style'
+
+export class Header extends Component {
+  render() {
+    return (
+      <HeaderWrapper>
+        Header
+      </HeaderWrapper>
+    )
+  }
+}
+
+export default Header
